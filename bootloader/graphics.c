f93070dda79f99ff40c9a91acc7946717eef2dab
Nicu <nicuborta@tutanota.com>
2022-11-25 20:22:27 +0200

    Jankily implement graphics

--- bootloader/graphics.c
+++ bootloader/graphics.c
@@ -11,13 +11,11 @@
 #include "efilib.h"
 #include "graphics.h"
 
-#define TEST_SCREEN_COL_NUM             4
-#define TEST_SCREEN_ROW_NUM             3
-#define TEST_SCREEN_TOTAL_TILES         TEST_SCREEN_COL_NUM * TEST_SCREEN_ROW_NUM
-#define TEST_SCREEN_PRIMARY_COLOUR      0x00FF4000
-#define TEST_SCREEN_SECONDARY_COLOUR    0x00FF80BF
-
-#define ERR_FIND_MODE L"graphics:find_mode()\r\n"
+#define TEST_COL_NUM             4
+#define TEST_ROW_NUM             3
+#define TEST_TOTAL_TILES         TEST_SCREEN_COL_NUM * TEST_SCREEN_ROW_NUM
+#define TEST_PRIMARY_COLOUR      0x00FF4000
+#define TEST_SECONDARY_COLOUR    0x00FF80BF
 
 /*
  * @brief Finds a video mode.
@@ -29,7 +27,7 @@
  * @param[in]  targetHeight		The target height to search for.
  * @param[in]  targetPixelFormat	The target pixel format to search for.
  *
- * @retval videoMode.
+ * @retval modeNum.
  */
 static UINT32
 find_mode(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol,
@@ -43,12 +41,95 @@ find_mode(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol,
 	for (UINT32 i = 0; i < protocol->MaxMode; i++){
 		status = protocol->QueryMode(protocol, i, &size, &modeInfo);
 		if (EFI_ERROR(status))
-			err_handle(status, ERR_FIND_MODE);
+			err_handle(status, L"graphics:find_mode:QueryMode\r\n");
 
 		if (modeInfo->HorizontalResolution == targetWidth &&
 			modeInfo->VerticalResolution == targetHeight &&
 			modeInfo->PixelFormat == targetPixelFormat)
 				return i;
 	}
-	err_handle(EFI_UNSUPPORTED, ERR_FIND_MODE);
+	err_handle(EFI_UNSUPPORTED, L"graphics:find_mode\r\n");
+}
+
+// TODO: Maybe move the above function to the below one
+void
+graphics_set_mode(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol,
+	const UINT32 targetWidth, const UINT32 targetHeight,
+	const EFI_GRAPHICS_PIXEL_FORMAT targetPixelFormat)
+{
+	EFI_STATUS status;
+	UINT32 modeNum = find_mode(protocol, targetWidth, targetHeight,
+		targetPixelFormat);
+
+	status = protocol->SetMode(protocol, modeNum);
+
+	if (EFI_ERROR(status))
+		err_handle(status, L"graphics:set_mode\r\n");
+}
+
+void
+graphics_init(struct Graphics *graphics)
+{
+	EFI_STATUS status;
+	EFI_GUID gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+
+	// Populate graphics service handle buffer.
+	status = BOOT_SERVICES->LocateHandleBuffer(ByProtocol,
+		&gopGuid, 0, &graphics_service->handleCount,
+		&graphics_service->handleBuffer);
+
+	if (EFI_ERROR(status))
+		err_handle(status, L"graphics:init\r\n");
+}
+
+void
+graphics_close(struct Graphics *graphics)
+{
+	EFI_STATUS status = BOOT_SERVICES->FreePool(
+		graphics_service.handle_buffer);
+
+	if (EFI_ERROR(status))
+		err_handle(status, L"graphics:close\r\n");
+}
+
+void
+graphics_draw_rect(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol,
+	const UINT16 x, const UINT16 y, const UINT16 width, const UINT16 height,
+	const UINT32 color)
+{
+	// Pointer to the current pixel in the buffer
+	UINT32 *at;
+
+	for (UINT16 row = 0; row < height; row++){
+		for (UINT16 col = 0; col < width; col++){
+			at = (UINT32 *)protocol->Mode->FrameBufferBase + x + col;
+			at += ((y + row) * protocol->Mode->Info->PixelsPerScanLine);
+
+			*at = color;
+		}
+	}
+}
+
+void
+graphics_draw_test(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol)
+{
+	const UINT16 tileWidth = protocol->Mode->Info->HorizontalResolution /
+		TEST_SCREEN_COL_NUM;
+	const UINT16 tileHeight = protocol->Mode->Info->VerticalResolution /
+		TEST_SCREEN_ROW_NUM;
+
+	UINT8 x, y;
+	UINT32 color;
+
+	for (UINT8 p = 0; p < TEST_TOTAL_TILES; p++){
+		x = p % TEST_COL_NUM;
+		y = p / TEST_COL_NUM;
+
+		color = TEST_PRIMARY_COLOUR;
+		if (((y % 2) + x) % 2)
+			color = TEST_SCREEN_SECONDARY_COLOUR;
+
+		graphics_draw_rect(protocol, tileWidth * x, tileHeight * y,
+			tileWidth, tileHeight, color);
+	}
 }
--- bootloader/graphics.h
+++ bootloader/graphics.h
@@ -14,35 +14,24 @@
 #include "efilib.h"
 
 /*
+ * TODO: !!!
+ * 1) Figure out where graphics protocol is defined and why is it not in graphics
+ * 2) why is handleCount stored in struct Graphics, nobody is using it.
+ * On second thought, not even handleBuffer is used anywhere, what is the
+ * purpose of this??
+ * 3) maybe place protocol in the struct instead?
+ */
+
+/*
  * @brief Graphics output service.
  * Holds variables necesssary for using the UEFI graphics output service.
  *
  * Is this an appropriate name or file to define it in?
  */
-struct Graphics_Service {
-	EFI_HANDLE *	handle_buffer;
-	UINTN		handle_count;
+struct Graphics {
+	EFI_HANDLE *	handleBuffer;
+	UINTN		handleCount;
 };
-
-/*
- * @brief Initialises the Graphics output service.
- * Initialises the graphics output service. Populates the handle buffer in the
- * graphics output service.
- * @return		The program status.
- * @retval EFI_SUCCESS	If the function executed successfully.
- * @retval other	Any other value is an EFI error code.
- */
-EFI_STATUS graphics_init_output_service(void);
-
-/*
- * @brief Closes the graphics output service.
- * Closes the graphics output service, freeing the handle buffer used by the
- * service.
- * @return		The program status.
- * @retval EFI_SUCCESS	If the function executed successfully.
- * @retval other	Any other value is an EFI error code.
- */
-EFI_STATUS graphics_close_output_service(void);
 
 /*
  * @brief Set the graphics mode for a particular protocol.
@@ -50,17 +39,29 @@ EFI_STATUS graphics_close_output_service(void);
  * mode by searching all available modes on this protocol for one that matches
  * the target width/height.
  * @param[in] protocol			The protocol to set the mode for.
- * @param[in] target_width		The target width.
- * @param[in] target_height		The target height.
- * @param[in] target_pixel_format	The target pixel format.
- * @return The program status.
- * @retval EFI_SUCCESS		If the function executed successfully.
- * @retval EFI_UNSUPPORTED	If the specified mode cannot be found.
- * @retval other		Any other value is an EFI error code.
+ * @param[in] targetWidth		The target width.
+ * @param[in] targetHeight		The target height.
+ * @param[in] targetPixelFormat		The target pixel format.
  */
-EFI_STATUS graphics_set_mode(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol,
-	const UINT32 target_width, const UINT32 target_height,
-	const EFI_GRAPHICS_PIXEL_FORMAT target_pixel_format);
+void graphics_set_mode(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol,
+	const UINT32 targetWidth, const UINT32 targetHeight,
+	const EFI_GRAPHICS_PIXEL_FORMAT targetPixelFormat);
+
+/*
+ * @brief Initialises the Graphics output service.
+ * Initialises the graphics output service. Populates the handle buffer in the
+ * graphics output service.
+ * @param[out] graphics
+ */
+void graphics_init(struct Graphics *graphics);
+
+/*
+ * @brief Closes the graphics output service.
+ * Closes the graphics output service, freeing the handle buffer used by the
+ * service.
+ * @param[in] graphics
+ */
+void graphics_close(struct Graphics *graphics);
 
 /*
  * @brief Draws a rectangle onto the framebuffer.
@@ -72,7 +73,7 @@ EFI_STATUS graphics_set_mode(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol,
  * @param[in] height	The height of the rectangle to draw.
  * @param[in] color	The color to draw.
  */
-void draw_rect(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol,
+void graphics_draw_rect(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol,
 	const UINT16 x, const UINT16 y, const UINT16 width, const UINT16 height,
 	const UINT32 color);
 
@@ -82,6 +83,6 @@ void draw_rect(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol,
  * protocol is functioning correctly. Draws to the provided framebuffer.
  * @param[in] protocol	The protocol containing the framebuffer to draw to.
  */
-void draw_test_screen(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol);
+void graphics_draw_test(EFI_GRAPHICS_OUTPUT_PROTOCOL * const protocol);
 
 #endif // GRAPHICS_H
